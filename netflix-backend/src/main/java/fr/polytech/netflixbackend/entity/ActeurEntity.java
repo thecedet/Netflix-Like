d2d4bff09b9c5a2d865bca8cddeee9970a1ba8f7
thecedet <thecedet@live.fr>
2023-12-03 17:38:12 +0100

    clean import

--- netflix-backend/src/main/java/fr/polytech/netflixbackend/entity/ActeurEntity.java
+++ netflix-backend/src/main/java/fr/polytech/netflixbackend/entity/ActeurEntity.java
@@ -2,14 +2,10 @@ package fr.polytech.netflixbackend.entity;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
